674e88c411c18cdd5155bebc9856189f87dead58
cy <cy@cyui-MacBookAir.local>
2022-03-23 13:58:45 +0900

    feature/mainpage

---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Navigation from "../organisms/Navigation";
+import styled from "styled-components";
+import Slick from "../molcules/Slick";
+
+function MainPage(props) {
+    return (
+        <div>
+            <Navigation />
+            <PageContainer>
+                <div className={'slick'}>
+                    <Slick />
+                </div>
+                <div style={{backgroundColor: 'white', height: '100%'}}>
+                    <h1>영상? 공지사항? 커뮤니티?</h1>
+                </div>
+            </PageContainer>
+        </div>
+    );
+}
+
+export default MainPage;
+
+const PageContainer = styled.div`
+  width: 100%;
+  height: 3000px;
+  padding-top: 160px;
+  background-color: black;
+  .slick {
+    margin-left: 35px;
+    margin-bottom: 100px;
+    width: 97%;
+  }
+`
+
