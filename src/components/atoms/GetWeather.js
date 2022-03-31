5e8afdf927693b6887d4b4e938f845dcb8926478
aa <jeheon12@git.ajou.ac.kr>
2022-03-31 18:25:17 +0900

    feature/GetWeather 위도 경도 API_URL

--- src/components/atoms/GetWeather.js
+++ src/components/atoms/GetWeather.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import getLocation from "./GetLocation";
 
 function GetWeather() {
-    const WEATHER_API_KEY = "a9c28c4ba75b36cf91e22559a5370c5a";
-    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?q=Suwon&appid=${WEATHER_API_KEY}&units=metric`;
     const [weatherStatus, setWeatherStatus] = useState('');
 
-    fetch(WEATHER_API_URL)
-        .then(response => response.json())
-        .then((data) => {
-            setWeatherStatus(data.weather[0].main);
-        });
+    useEffect(() => {
+        let lat, lon;
+        getLocation().then(res => {
+            lat = res.coords.latitude;
+            lon = res.coords.longitude;
+            const WEATHER_API_KEY = "a9c28c4ba75b36cf91e22559a5370c5a";
+            const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
 
+        fetch(WEATHER_API_URL)
+            .then(response => response.json())
+            .then((data) => {
+                setWeatherStatus(data.weather[0].main);
+            });
+        })
+    }, []);
     return (
         <div>{weatherStatus}</div>
     )
