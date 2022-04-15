171ce1f825dac024881e5c587d62b9231492f4cf
cy <cy@cyui-MacBookAir.local>
2022-04-15 20:47:46 +0900

    다이어리 저장하면 뮤직페이지로 이동

--- src/components/pages/MusicBehaviorPage.js
+++ src/components/pages/MusicBehaviorPage.js
@@ -16,7 +16,7 @@ function MusicBehaviorPage(props) {
             <PageContainer>
                 <div style={{ marginBottom: '20%'}}>
                         <div style={{width: '100%'}}>
-                            <Fade direction={"up"} cascade={false}>
+                            <Fade direction={"up"} cascade>
                                 <h1 style={{color: 'white', marginLeft: 80, fontSize: '5vw'}}>기분 좋을 때,</h1>
                                 <div style={{display: 'flex', justifyContent: 'center'}}>
                                     <img style={{width: '30%'}} src={smile1}/>
@@ -30,26 +30,25 @@ function MusicBehaviorPage(props) {
                 </div>
 
                 <div style={{display: 'flex'}}>
-                    <div style={{ marginLeft: '5%', marginTop: '2%', color: 'white', backgroundColor: 'black', width: '50%'}}>
+                    <div style={{ marginLeft: '5%', marginTop: '2%', marginBottom: '10%', color: 'white', backgroundColor: 'black', width: '50%'}}>
                         <Fade direction={"down"} cascade={true}>
                             <div style={{display: 'flex'}}>
-                                <img style={{width: '40%'}} src={music1}/>
-                                <img style={{width: '40%'}} src={music2}/>
+                                <img style={{width: '30%'}} src={music1}/>
+                                <img style={{width: '30%'}} src={music2}/>
                             </div>
                             <div style={{display: 'flex'}}>
-                                <img style={{width: '40%'}} src={music3}/>
-                                <img style={{width: '40%'}} src={music4}/>
+                                <img style={{width: '30%'}} src={music3}/>
+                                <img style={{width: '30%'}} src={music4}/>
                             </div>
                         </Fade>
                     </div>
                     <div style={{width: '40%', marginTop: '5%'}}>
-                        <Fade direction={"up"} cascade={true}>
+                        <Fade direction={"down"} cascade={true}>
                             <h1 style={{color: 'white', marginLeft: 80, fontSize: 50}}>혹은 운동과 함께</h1>
                             <h1 style={{color: 'white', marginLeft: 80, fontSize: 50}}>음악들을 감상해보세요.</h1>
                         </Fade>
                     </div>
                 </div>
-
             </PageContainer>
         </div>
     );
@@ -59,7 +58,7 @@ export default MusicBehaviorPage;
 
 const PageContainer = styled.div`
   width: 100%;
-  height: 3000px;
+  //height: 3000px;
   padding-top: 120px;
   background-color: black;
   .slick {
