5e8a069cc1bf8131a9a01a3b5c74b3c90ec8786f
aa <jeheon12@git.ajou.ac.kr>
2022-04-30 03:21:38 +0900

    Mypage get api 주석처리

--- src/components/pages/MyPage.js
+++ src/components/pages/MyPage.js
@@ -1,9 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navigation from "../organisms/Navigation";
 import styled from "styled-components";
 import CustomCalendar from '../atoms/CustomCalendar';
+import axios from 'axios';
 
 function MyPage(props) {
+
+    // const [userData, setUserData] = useState();
+
+    // const getData = async () => {
+    //     try {
+    //         setUserData(null);
+    //         const res = await axios.get('URL', {
+    //             parameter: {
+    //                 date: '달력 클릭한 해당 날짜??'
+    //             }
+    //         });
+    //         setUserData(res.data);
+    //     } catch (e) {
+    //         console.log('Error');
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     getData();
+    // }, []);
+
+    // if (!userData) return null;
+
     return (
         <div id="0">
             <Navigation />
@@ -12,8 +36,22 @@ function MyPage(props) {
                     <h1 style={{ color: 'white', fontSize: 50 }}>마이 페이지</h1>
                     <h2 style={{ color: '#AE946A' }}>한달간의 감정, 음악 통계를 확인해보세요</h2>
                 </div>
-                <div style={{ marginLeft: 80 }}>
+
+                <div style={{ marginLeft: 80, display: 'flex', flexDirection: 'column' }}>
                     <CustomCalendar />
+
+
+
+                    {/* <div> 날짜별 유저 정보 한줄로 출력
+                        {userData.map(user => (
+                            <div key={user.date} style={{ margin: '10px' }}>
+                                {user.id} {user.title}
+                            </div>
+                        ))}
+                    </div> */}
+
+
+
                 </div>
             </PageContainer>
         </div>
